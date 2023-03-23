5ffebf471451908f217ffdc70244c2454b96de20
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2023-03-23 11:51:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-//your code here
+let bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+// function to remove articles from the beginning of the string
+function removeArticle(str) {
+  let articles = ["a", "an", "the"];
+  let words = str.split(" ");
+  if (articles.includes(words[0].toLowerCase())) {
+    return words.slice(1).join(" ");
+  } else {
+    return str;
+  }
+}
+
+// sort the band names in lexicographic order
+bandNames.sort(function(a, b) {
+  a = removeArticle(a);
+  b = removeArticle(b);
+  return a.localeCompare(b);
+});
+
+// get the ul element by id
+let ul = document.getElementById("band");
+
+// create li elements for each band name and append to ul
+for (let i = 0; i < bandNames.length; i++) {
+  let li = document.createElement("li");
+  li.textContent = bandNames[i];
+  ul.appendChild(li);
+}
